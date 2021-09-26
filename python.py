993358eb9f6f28e5a8efe0f2d4e3d39969912780
minbanyartalahtaw <91402522+minbanyartalahtaw@users.noreply.github.com>
2021-09-26 16:14:49 +0630

    Update python.py

--- python.py
+++ python.py
@@ -6,3 +6,19 @@ a = input("Enter 0")
 
 if a == "0":
     print("Hello World")
+
+
+    ##hello my name is banyar
+#My name is oakkar
+a = input("Enter 0")
+
+if a == "0":
+    print("Hello World")
+
+
+    ##hello my name is banyar
+#My name is oakkar
+a = input("Enter 0")
+
+if a == "0":
+    print("Hello World")
